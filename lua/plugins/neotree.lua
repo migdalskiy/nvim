a0a8aa1d13376cc51174da11ad6085ac5b4f75b5
Sergiy Migdalskiy <migdalskiy@hotmail.com>
2024-11-20 16:54:51 -0800

    added "CWD" key mappings to current buffer and neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,12 +1,21 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true,
-                hide_dotfiles = false,
-                hide_gitignored = false,
-            },
-        },
-    },
+    opts = function(_, opts)
+        opts.filesystem = opts.filesystem or {}
+        opts.filesystem.filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+        }
+        opts.window = opts.window or {}
+        opts.window.mappings = opts.window.mappings or {}
+        opts.window.mappings["J"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" then
+                vim.cmd("cd " .. node.path)
+                vim.notify("Changed directory to: " .. node.path, vim.log.levels.INFO)
+            end
+        end
+        return opts
+    end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -7,6 +7,31 @@ local function p4_cmd(cmd)
     end
 end
 
+function TogglePyrightDiagnostics()
+    local current_client = vim.lsp.get_active_clients({ name = "pyright" })[1]
+    if current_client then
+        if current_client.server_capabilities.diagnosticProvider then
+            vim.diagnostic.disable(0)
+            print("Pyright diagnostics disabled")
+        else
+            vim.diagnostic.enable(0)
+            print("Pyright diagnostics enabled")
+        end
+    else
+        print("Pyright is not attached to this buffer")
+    end
+end
+
+function ChangeCwdToBufferDir()
+    local buf_dir = vim.fn.expand("%:p:h")
+    if buf_dir ~= "" then
+        vim.cmd("cd " .. buf_dir)
+        print("Changed directory to: " .. buf_dir)
+    else
+        print("No file path found for current buffer.")
+    end
+end
+
 -- Setup which-key mapping
 --wk.setup({})
 
@@ -38,7 +63,9 @@ return {
             desc = "Perforce Add v1",
         },
         { "<leader>pa", p4_cmd("add"), desc = "P4 Add File" },
-        { "<leader>pb", p4_cmd("edit"), desc = "P4 Edit File" },
+        { "<leader>pe", p4_cmd("edit"), desc = "P4 Edit File" },
         { "<leader>pr", p4_cmd("revert"), desc = "P4 Revert File" },
+        { "<leader>ct", TogglePyrightDiagnostics, desc = "Toggle Pyright Diagnistics" },
+        { "<leader>pf", ChangeCwdToBufferDir, desc = "Change cwd to Buffer Dir" },
     },
 }
