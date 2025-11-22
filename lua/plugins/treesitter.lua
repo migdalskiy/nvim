7e9a22abbb80a62ca1b05ef7aa802b106dfddd1b
Sergiy Migdalskiy <migdalskiy@hotmail.com>
2025-11-22 19:11:37 -0300

    updated treesitter and minianimate to newer version. installed nvm, node, npm, nvim-treesitter-cli and now starting nvim under MSVC vars in Windows, and everything started working again

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,18 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+
     opts = {
       ensure_installed = {
         "lua",
         "python",
         "javascript",
-        -- Add more languages as needed
       },
       highlight = { enable = true },
       indent = { enable = true },
-      -- Add more Treesitter configurations here
     },
   },
 }
