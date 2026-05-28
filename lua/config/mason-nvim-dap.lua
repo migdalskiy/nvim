1d8e0c94950302e5d969994baecc11b49979fdcc
Sergiy Migdalskiy <migdalskiy@hotmail.com>
2026-05-28 14:11:41 -0700

    preparing for codelldb debugging

--- lua/config/mason-nvim-dap.lua
+++ lua/config/mason-nvim-dap.lua
@@ -3,7 +3,11 @@ local get_debugpy = require("util.debugpy_path").get_debugpy
 return {
   "jay-babu/mason-nvim-dap.nvim",
   opts = {
+    -- Ensure both debuggers are installed automatically by Mason
+    ensure_installed = { "python", "codelldb" },
+
     handlers = {
+      -- Your existing custom Python setup
       python = function()
         local path = get_debugpy()
 
@@ -12,6 +16,12 @@ return {
           command = path,
           args = { "-m", "debugpy.adapter" },
         }
+      end,
+
+      -- The default handler catches codelldb (and any other adapter in ensure_installed)
+      -- and sets up the standard configuration automatically.
+      function(config)
+        require("mason-nvim-dap").default_setup(config)
       end,
     },
   },
