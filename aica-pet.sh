b4f36002dff0df8434d687c9133704382916f048
Sergiy Migdalskiy <migdalskiy@hotmail.com>
2026-09-14 15:27:46 -0700

    some renames

--- aica-pet.sh
+++ aica-pet.sh
@@ -2,8 +2,8 @@
 #podman build  -t aica:latest .
 
 podman run \
-  -p 0.0.0.0:3000:3000 -p 0.0.0.0:3001:3001 -p 0.0.0.0:8301:8301 -p 0.0.0.0:8300:8300 -p 0.0.0.0:5002:5002 \
-  --name aica-pet --hostname aica-pet \
+  -p 0.0.0.0:3000:3000 -p 0.0.0.0:3001:3001 -p 0.0.0.0:8301:8301 -p 0.0.0.0:8300:8300 -p 0.0.0.0:8031:8031 -p 0.0.0.0:5002:5002 \
+  --name aica --hostname aica-pet \
   -v ~/.debug:/home/dev/.debug:z \
   -v ~/pet:/app:z \
   -v nvim_data:/home/dev/.local/share/nvim:z \
